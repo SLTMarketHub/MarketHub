277971280b135ebd78622acedd156b1a075853a9
Madhusha <mdweerasiri@gmail.com>
2025-09-11 02:10:29 +0530

    removed unwanted files for the branch

--- apps/backend/TMF629_Customer_API/Models/EventHub.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const EventHubSchema = new mongoose.Schema({
-    callback: { type: String, required: true }
-});
-
-module.exports = mongoose.model('EventHub', EventHubSchema);
