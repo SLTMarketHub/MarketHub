214ecf326fda52ba31892bac1e565de11544e846
Madhusha <mdweerasiri@gmail.com>
2025-09-11 01:27:34 +0530

    port changed

--- apps/backend/TMF681_Communication_Management_API_/server.js
+++ apps/backend/TMF681_Communication_Management_API_/server.js
@@ -15,8 +15,8 @@ app.use(cors());
 app.use(express.json());
 
 
-app.use("/tmf-api/communicationManagement/v4/communicationMessage", communicationRoutes);
-app.use("/", hubRoutes);
+app.use("/tmf-api/communicationManagement/v4", communicationRoutes);
+app.use("/tmf-api/communicationManagement/v4", hubRoutes);
 
 
 const PORT = process.env.PORT || 5008;
