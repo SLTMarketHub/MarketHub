cc59e0238b1828160e0d359af1f4b6fd2d084478
Madhusha <mdweerasiri@gmail.com>
2025-09-15 11:51:04 +0530

    server.js updated to align with TM Forum

--- apps/backend/TMF681_Communication_Management_API_/server.js
+++ apps/backend/TMF681_Communication_Management_API_/server.js
@@ -15,8 +15,8 @@ app.use(cors());
 app.use(express.json());
 
 
-app.use("/communicationMessage/", communicationRoutes);
-app.use("/hub/", hubRoutes);
+app.use("/communicationMessage", communicationRoutes);
+app.use("/hub", hubRoutes);
 
 
 const PORT = process.env.PORT || 5008;
