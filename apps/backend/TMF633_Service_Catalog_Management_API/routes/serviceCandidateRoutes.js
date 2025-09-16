d3fdc364a238cf183d6a290c9209dd1d06b7c279
Madhusha <mdweerasiri@gmail.com>
2025-09-16 19:44:08 +0530

    fixed a typo in TMF633/routes

--- apps/backend/TMF633_Service_Catalog_Management_API/routes/serviceCandidateRoutes.js
+++ apps/backend/TMF633_Service_Catalog_Management_API/routes/serviceCandidateRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const ServiceCandidate = require('../models/serviceCandidate');
+const ServiceCandidate = require('../models/ServiceCandidate');
 
 // POST
 router.post('/', async (req, res) => {
