7177931859bfa4b02c97a2c9504a45502a7d56c7
Madhusha <mdweerasiri@gmail.com>
2025-09-16 18:22:23 +0530

    Removed all files inside backend service folders

--- apps/backend/TMF678_Customer_Bill_API/models/Hub.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const HubSchema = new mongoose.Schema({
-  callback: { type: String, required: true },
-  query: { type: String, default: "" },
-}, { timestamps: true });
-
-module.exports = mongoose.model("Hub", HubSchema);
