a1bfc8e9327ee59bcb254ca8f4297612e59b8b9f
Madhusha Weerasiri <mdweerasiri@gmail.com>
2025-09-17 17:03:44 +0530

    Update app.js

--- apps/backend/auth-service/app.js
+++ apps/backend/auth-service/app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(passport.initialize());
 
-app.use('/authService', authRoutes);
+app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
 
 app.use(errorHandler);
