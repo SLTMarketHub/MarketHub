f786359b9c44d2aa34586136a86b838040ddc6a8
deshini kanchana <deshinisampath@gmail.com>
2025-09-17 16:22:56 +0530

    Update Auth Service endpoints to use deployed Render base URLs instead of localhost

--- apps/backend/auth-service/app.js
+++ apps/backend/auth-service/app.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const connectDB = require('./Config/db');
 const userRoutes = require('./Route/userRoute');
+const authRoutes = require('./Route/authRoutes')
 const { errorHandler } = require('./Middleware/errorMiddleware');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
@@ -18,13 +19,13 @@ const app = express();
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
 
-app.use('/tmf-api/auth', require('./Route/authRoutes'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(passport.initialize());
 
-app.use('/tmf-api/users', userRoutes);
+app.use('/authService', authRoutes);
+app.use('/users', userRoutes);
 
 app.use(errorHandler);
 
