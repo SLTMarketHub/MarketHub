98c2dc59afc2149dbd7e1484451a06b294bdb258
deshini kanchana <deshinisampath@gmail.com>
2025-11-10 22:54:18 +0530

    Corrected Send OTP function behavior

--- apps/backend/auth-service/Controller/authController.js
+++ apps/backend/auth-service/Controller/authController.js
@@ -259,7 +259,7 @@ exports.sendOTP = async (req, res) => {
     res.json({ message: "OTP sent successfully" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Failed to send OTP", error: error.message });
+    res.status(500).json({ message: "OTP Is Not Send", error: error.message });
   }
 };
 
